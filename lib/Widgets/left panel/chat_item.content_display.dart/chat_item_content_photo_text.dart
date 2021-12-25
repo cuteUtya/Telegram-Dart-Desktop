76aa75d7af2d6bef7f92320bd8d072e46c015fde
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2021-12-25 13:30:54 +0200

    minor layout fix in ChatItemContentPhotoText

--- lib/Widgets/left panel/chat_item.content_display.dart/chat_item_content_photo_text.dart
+++ lib/Widgets/left panel/chat_item.content_display.dart/chat_item_content_photo_text.dart
@@ -4,11 +4,9 @@ import 'package:myapp/Widgets/display_text.dart';
 class ChatItemContentPhotoText {
   static List<InlineSpan> build(Widget? photo, String text) {
     return [
-      WidgetSpan(
-          child: photo == null
-              ? const SizedBox.shrink()
-              : SizedBox(width: 20, height: 20, child: photo)),
-      const WidgetSpan(child: SizedBox(width: 4)),
+      if (photo != null)
+        WidgetSpan(child: SizedBox(width: 20, height: 20, child: photo)),
+      if (photo != null) const WidgetSpan(child: SizedBox(width: 4)),
       TextSpan(text: text, style: TextDisplay.chatItemAccent),
       const WidgetSpan(child: SizedBox(width: 8))
     ];
