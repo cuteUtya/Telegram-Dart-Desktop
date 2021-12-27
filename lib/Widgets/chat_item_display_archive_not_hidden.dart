f2974a85562c1c07546712542713c516eed28988
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2021-12-27 15:45:45 +0200

    Code impovments in ChatItemDisplayArchiveNotHidden

--- lib/Widgets/chat_item_display_archive_not_hidden.dart
+++ lib/Widgets/chat_item_display_archive_not_hidden.dart
@@ -2,44 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:myapp/ThemesEngine/theme_interpreter.dart';
 import 'package:myapp/Widgets/left%20panel/chat_item_base.dart';
 import 'package:myapp/Widgets/display_text.dart';
-import 'package:myapp/Widgets/left%20panel/chat_lists_manager.dart';
 import 'package:myapp/Widgets/unread_mention_bubble.dart';
 import 'package:myapp/tdlib/client.dart';
-import 'package:collection/collection.dart';
 import 'package:myapp/tdlib/src/tdapi/tdapi.dart' hide Text hide RichText;
+import 'package:myapp/utils.dart';
 
 class ChatItemDisplayArchiveNotHidden extends StatelessWidget {
   const ChatItemDisplayArchiveNotHidden(
       {Key? key, required this.client, required this.chats, this.onClick})
       : super(key: key);
   final TelegramClient client;
-  final List<ChatFullInfo> chats;
+  final List<Chat> chats;
   final Function()? onClick;
   @override
   Widget build(BuildContext context) {
     List<InlineSpan> content = [];
-    var c = chats.where((element) {
-      var archivePos = element.chat.positions!
-          .firstWhereOrNull((e) => e.list is ChatListArchive);
-      return archivePos != null && archivePos.order != 0;
-    });
-    c.forEach((element) => content.addAll(TextDisplay.parseEmojiInString(
-            element.chat.title! + (element.chat.unreadCount! <= 0 ? ", " : ""),
-            TextDisplay.create(
-                size: 18,
-                fontWeight: (element.chat.unreadCount ?? 0) <= 0
-                    ? FontWeight.normal
-                    : FontWeight.bold)) +
-        [
-          const WidgetSpan(child: SizedBox(width: 4)),
-          WidgetSpan(
-              child: UnreadCountBubble(
-                  fontSize: 14,
-                  count: element.chat.unreadCount!,
-                  important: false /*TODO true if have notify */)),
-          if (element.chat.unreadCount! > 0)
-            TextSpan(text: ", ", style: TextDisplay.create(size: 18)),
-        ]));
+    var c = chats.where(
+        (element) => showChatInChatList(element.positions!, ChatListArchive()));
+    for (var element in c) {
+      content.addAll(TextDisplay.parseEmojiInString(
+              element.title! + (element.unreadCount! <= 0 ? ", " : ""),
+              TextDisplay.create(
+                  size: 18,
+                  fontWeight: (element.unreadCount ?? 0) <= 0
+                      ? FontWeight.normal
+                      : FontWeight.bold)) +
+          [
+            const WidgetSpan(child: SizedBox(width: 4)),
+            WidgetSpan(
+                child: UnreadCountBubble(
+                    fontSize: 14,
+                    count: element.unreadCount!,
+                    important: false /*TODO true if have notify */)),
+            if (element.unreadCount! > 0)
+              TextSpan(text: ", ", style: TextDisplay.create(size: 18)),
+          ]);
+    }
     return ChatItemBase(
         selected: false,
         onClick: onClick,
