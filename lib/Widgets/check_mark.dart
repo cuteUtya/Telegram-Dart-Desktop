1b4dfdb2eb367e431fdb8fa346b267b075134652
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2022-01-03 18:27:31 +0200

    Add ability to use custom color for CheckMark

--- lib/Widgets/check_mark.dart
+++ lib/Widgets/check_mark.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:myapp/ThemesEngine/theme_interpreter.dart';
 
 class CheckMark extends StatelessWidget {
-  const CheckMark({Key? key, required this.isReaded}) : super(key: key);
+  const CheckMark({Key? key, required this.isReaded, this.color})
+      : super(key: key);
   final bool isReaded;
+  final Color? color;
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Icon(isReaded ? Icons.done_all : Icons.done,
-          color: ClientTheme.currentTheme.getField("CheckMarkChatListColor")),
-      const SizedBox(width: 2)
-    ]);
+    return Icon(isReaded ? Icons.done_all : Icons.done,
+        color: color ??
+            ClientTheme.currentTheme.getField("CheckMarkChatListColor"));
   }
 }
