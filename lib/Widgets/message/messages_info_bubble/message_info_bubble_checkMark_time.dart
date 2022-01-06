608bc215043f428af1c26b52b8cd64713fdc016e
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2022-01-06 17:45:58 +0200

    Add ability to use background in MessageInfoBubbleCheckMarkTime

--- lib/Widgets/message/messages_info_bubble/message_info_bubble_checkMark_time.dart
+++ lib/Widgets/message/messages_info_bubble/message_info_bubble_checkMark_time.dart
@@ -2,22 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:myapp/ThemesEngine/theme_interpreter.dart';
 import 'package:myapp/Widgets/check_mark.dart';
 import 'package:myapp/Widgets/display_text.dart';
+import 'package:myapp/Widgets/message/messages_info_bubble/message_info_bubble_base.dart';
 
 class MessageInfoBubbleCheckMarkTime extends StatelessWidget {
   const MessageInfoBubbleCheckMarkTime(
       {Key? key,
       this.checkMarkValue,
       required this.time,
-      this.isOutgoing = false})
+      this.isOutgoing = false,
+      this.hasBackground = false})
       : super(key: key);
   final bool? checkMarkValue;
   final String time;
   final bool isOutgoing;
+  final bool hasBackground;
   @override
   Widget build(BuildContext context) {
-    var color = ClientTheme.currentTheme
-        .getField(isOutgoing ? "CheckMarkMineColor" : "CheckMarkNotMineColor");
-    return Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+    var color = ClientTheme.currentTheme.getField(hasBackground
+        ? "CheckMarkInFlowContainer"
+        : (isOutgoing ? "CheckMarkMineColor" : "CheckMarkNotMineColor"));
+    var content = Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
       Text(
         time,
         style: TextDisplay.create(size: 16, customTextColor: color),
@@ -26,5 +30,8 @@ class MessageInfoBubbleCheckMarkTime extends StatelessWidget {
       if (checkMarkValue != null)
         CheckMark(isReaded: checkMarkValue!, color: color, size: 20)
     ]);
+
+    if (hasBackground) return MessageInfoBubbleBase(content: content);
+    return content;
   }
 }
