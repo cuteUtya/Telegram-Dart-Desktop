9059b292bba0083d5c28078008ea0abd442cb09f
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2022-01-06 18:45:55 +0200

    Special look for text nessages what contains only emojis

--- lib/Widgets/message/message_display.dart
+++ lib/Widgets/message/message_display.dart
@@ -4,10 +4,13 @@ import 'package:myapp/Widgets/Userpic/userpic.dart';
 import 'package:myapp/Widgets/display_text.dart';
 import 'package:myapp/Widgets/message/mac_message_bubble.dart';
 import 'package:myapp/Widgets/message/message_display_text.dart';
+import 'package:myapp/Widgets/message/message_display_text_emojis.dart';
+import 'package:myapp/Widgets/message/messages_info_bubble/message_info_bubble_checkMark_time.dart';
 import 'package:myapp/tdlib/client.dart';
 import 'package:myapp/tdlib/td_api.dart' hide Text;
 import 'package:myapp/tdlib/tdlibUtils.dart';
 import 'package:myapp/Widgets/message/bubble_utils.dart';
+import 'package:myapp/utils.dart';
 
 /// UI representation of [Message] object
 /// * [message] message object
@@ -24,6 +27,34 @@ class MessageDisplay extends StatelessWidget {
     Widget contentWidget;
     switch (message.content.runtimeType) {
       case MessageText:
+        var contentText = message.content as MessageText;
+        var textUnwhitespaced = contentText.text!.text!.replaceAll(" ", "");
+        var emojiTest = emojiRegex.firstMatch(textUnwhitespaced);
+        if (emojiTest != null) {
+          /*
+          if message contains only emojis and its count not much 
+          show its without bubble and with big size, like Tdesktop or TelegramX
+          */
+          var totalEmojis = emojiTest.end - emojiTest.start;
+          if (totalEmojis >= textUnwhitespaced.length && totalEmojis < 40) {
+            contentWidget = MessageDisplayTextEmojis(
+                alignment: message.isOutgoing!
+                    ? MainAxisAlignment.end
+                    : MainAxisAlignment.start,
+                emojis: textUnwhitespaced,
+                infoSide: message.isOutgoing! ? Side.left : Side.right,
+                messageInfo: MessageInfoBubbleCheckMarkTime(
+                  useBackground: true,
+                  isOutgoing: message.isOutgoing!,
+                  time: getHHMM(unixToDateTime(message.date!)),
+                  checkMarkValue: message.isOutgoing!
+                      ? message.id! <= (chat?.lastReadOutboxMessageId ?? 0)
+                      : null,
+                ));
+            break;
+          }
+        }
+
         contentWidget = MessageDisplayText(
           userPost: message.authorSignature,
           senderName: (chat?.type is ChatTypeSupergroup ||
@@ -35,6 +66,7 @@ class MessageDisplay extends StatelessWidget {
           lastReadOutboxMessageId: chat?.lastReadOutboxMessageId ?? 0,
           message: message,
         );
+
         break;
       default:
         contentWidget = MacMessageBubble(
