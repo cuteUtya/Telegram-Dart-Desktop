2c33226d168fdc15fb846e26e97d14003236cc84
Tim-dev-hub <61709763+Tim-dev-hub@users.noreply.github.com>
2022-01-26 20:52:42 +0200

    Add ability to use new microsoft emojis

--- lib/Widgets/display_text.dart
+++ lib/Widgets/display_text.dart
@@ -5,7 +5,7 @@ import 'package:myapp/tdlib/td_api.dart' hide Text;
 import 'package:myapp/utils.dart';
 
 class TextDisplay {
-  static String _getEmojiFont() => "TwitterColorEmoji";
+  static String _getEmojiFont() => "MicrosoftColorEmoji";
 
   static TextStyle get title =>
       create(size: 24, fontWeight: FontWeight.w600, textColor: ClientTheme.currentTheme.getField("HeaderMain"));
