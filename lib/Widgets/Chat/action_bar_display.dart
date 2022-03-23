2d86f74849367295177d98c14588a24f79784a20
Tim-dev-hub <timurteresenko@gmail.com>
2022-03-23 20:49:01 +0200

    Add ability to blur action bar

--- lib/Widgets/Chat/action_bar_display.dart
+++ lib/Widgets/Chat/action_bar_display.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:myapp/State managment/ui_events.dart';
 import 'package:myapp/Themes engine/theme_interpreter.dart';
@@ -13,106 +15,149 @@ class ActionBarDisplay extends StatelessWidget {
     Key? key,
     required this.client,
     required this.chat,
+    required this.useBlurStyle,
     this.onChatRevert,
   }) : super(key: key);
 
   final TelegramClient client;
   final Chat chat;
   final VoidCallback? onChatRevert;
+  final bool useBlurStyle;
   @override
   Widget build(BuildContext context) {
     User? user = getInterlocutor(chat, client);
-    Supergroup? supergroup =
-        chat.type is ChatTypeSupergroup ? client.getSupergroup((chat.type as ChatTypeSupergroup).supergroupId!) : null;
-    BasicGroup? group =
-        chat.type is ChatTypeBasicGroup ? client.getBasicGroup((chat.type as ChatTypeBasicGroup).basicGroupId!) : null;
+    Supergroup? supergroup = chat.type is ChatTypeSupergroup
+        ? client.getSupergroup((chat.type as ChatTypeSupergroup).supergroupId!)
+        : null;
+    BasicGroup? group = chat.type is ChatTypeBasicGroup
+        ? client.getBasicGroup((chat.type as ChatTypeBasicGroup).basicGroupId!)
+        : null;
     int membersCount = (group?.memberCount ?? supergroup?.memberCount) ?? 0;
-    return Stack(alignment: Alignment.centerLeft, children: [
-      Container(
-        height: 64,
-        alignment: Alignment.center,
-        color: ClientTheme.currentTheme.getField("ActionBarColor"),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 4),
+
+    const double height = 64;
+
+    var blurSize = const SizedBox(
+      width: double.infinity,
+      height: height,
+    );
+
+    var title = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 4),
+          StreamBuilder(
+            stream: client.senderName(MessageSenderChat(chatId: chat.id)),
+            initialData: client.getChatTitleSync(chat.id!),
+            builder: (_, data) => ChatItemTitle(
+              textColor:
+                  ClientTheme.currentTheme.getField("ActionBarTitleColor"),
+              selected: false,
+              isBot: user?.type is UserTypeBot,
+              isChannel: supergroup?.isChannel ?? false,
+              isChat: !(supergroup?.isChannel ?? true),
+              title: data.data.toString(),
+              isScam: user?.isScam ?? false || (supergroup?.isScam ?? false),
+              isVerifed: supergroup?.isVerified ?? false,
+              isSupport: user?.isSupport ?? false,
+            ),
+          ),
+          const SizedBox(height: 2),
+          if (supergroup?.isChannel ?? false)
+            client.buildTextByKey(
+              "lng_chat_status_subscribers",
+              TextDisplay.actionBarOffline,
+              replacing: {"{count}": (supergroup?.memberCount ?? 0).toString()},
+              itemsCount: supergroup?.memberCount ?? 0,
+            )
+          else if (user?.status != null)
             StreamBuilder(
-              stream: client.senderName(MessageSenderChat(chatId: chat.id)),
-              initialData: client.getChatTitleSync(chat.id!),
-              builder: (_, data) => ChatItemTitle(
-                textColor: ClientTheme.currentTheme.getField("ActionBarTitleColor"),
-                selected: false,
-                isBot: user?.type is UserTypeBot,
-                isChannel: supergroup?.isChannel ?? false,
-                isChat: !(supergroup?.isChannel ?? true),
-                title: data.data.toString(),
-                isScam: user?.isScam ?? false || (supergroup?.isScam ?? false),
-                isVerifed: supergroup?.isVerified ?? false,
-                isSupport: user?.isSupport ?? false,
-              ),
+              initialData: user!.status,
+              stream: client.statusOf(user.id!),
+              builder: (_, data) => data.hasData
+                  ? _buildStatusText(
+                      data.data as UserStatus, user.type is UserTypeBot)
+                  : const SizedBox.shrink(),
+            )
+          else
+            StreamBuilder(
+              stream: client.onlineMemebersIn(chat.id!),
+              builder: (_, data) {
+                var onlineCount = (data.data ?? 0) as int;
+                return client.buildTextByKey(
+                  onlineCount > 0
+                      ? "lng_chat_status_members_online"
+                      : "lng_chat_status_members",
+                  TextDisplay.actionBarOffline,
+                  itemsCount: onlineCount,
+                  replacing: {
+                    "{count}": membersCount.toString(),
+                    "{members_count}": client.getTranslation(
+                        "lng_channel_members_link",
+                        replacing: {"{count}": membersCount.toString()},
+                        itemsCount: membersCount),
+                    "{online_count}": client.getTranslation(
+                        "lng_chat_status_online",
+                        replacing: {"{count}": onlineCount.toString()},
+                        itemsCount: onlineCount),
+                  },
+                );
+              },
             ),
-            const SizedBox(height: 2),
-            if (supergroup?.isChannel ?? false)
-              client.buildTextByKey(
-                "lng_chat_status_subscribers",
-                TextDisplay.actionBarOffline,
-                replacing: {"{count}": (supergroup?.memberCount ?? 0).toString()},
-                itemsCount: supergroup?.memberCount ?? 0,
-              )
-            else if (user?.status != null)
-              StreamBuilder(
-                initialData: user!.status,
-                stream: client.statusOf(user.id!),
-                builder: (_, data) =>
-                    data.hasData ? _buildStatusText(data.data as UserStatus, user.type is UserTypeBot) : const SizedBox.shrink(),
-              )
-            else
-              StreamBuilder(
-                stream: client.onlineMemebersIn(chat.id!),
-                builder: (_, data) {
-                  var onlineCount = (data.data ?? 0) as int;
-                  return client.buildTextByKey(
-                    onlineCount > 0 ? "lng_chat_status_members_online" : "lng_chat_status_members",
-                    TextDisplay.actionBarOffline,
-                    itemsCount: onlineCount,
-                    replacing: {
-                      "{count}": membersCount.toString(),
-                      "{members_count}": client.getTranslation("lng_channel_members_link",
-                          replacing: {"{count}": membersCount.toString()}, itemsCount: membersCount),
-                      "{online_count}": client.getTranslation("lng_chat_status_online",
-                          replacing: {"{count}": onlineCount.toString()}, itemsCount: onlineCount),
-                    },
-                  );
-                },
-              ),
-          ],
-        ),
+        ],
       ),
-      Container(
-        width: 48,
-        height: 48,
-        margin: const EdgeInsets.only(left: 12),
-        child: TextButton(
-            style: ButtonStyle(
-              overlayColor: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.pressed)) {
-                  return ClientTheme.currentTheme.getField("CloseChatClickColor");
-                }
-                return Colors.transparent;
-              }),
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                ),
+    );
+
+    var closeBtn = Container(
+      width: 48,
+      height: 48,
+      margin: const EdgeInsets.only(left: 12),
+      child: TextButton(
+          style: ButtonStyle(
+            overlayColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return ClientTheme.currentTheme.getField("CloseChatClickColor");
+              }
+              return Colors.transparent;
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
               ),
             ),
-            child: Icon(
-              Icons.arrow_back,
-              color: ClientTheme.currentTheme.getField("CloseChatIconColor"),
-            ),
-            onPressed: onChatRevert),
-      )
-    ]);
+          ),
+          child: Icon(
+            Icons.arrow_back,
+            color: ClientTheme.currentTheme.getField("CloseChatIconColor"),
+          ),
+          onPressed: onChatRevert),
+    );
+
+    var blur = ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          //TODO get this values from settings
+          sigmaX: 10,
+          sigmaY: 10,
+        ),
+        child: blurSize,
+      ),
+    );
+
+    return Container(
+      height: height,
+      color: useBlurStyle
+          ? null
+          : ClientTheme.currentTheme.getField("ActionBarColor"),
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          if (useBlurStyle) blur,
+          title,
+          closeBtn,
+        ],
+      ),
+    );
   }
 
   Widget _buildStatusText(UserStatus status, bool isBot) {
@@ -121,19 +166,29 @@ class ActionBarDisplay extends StatelessWidget {
       switch (status.runtimeType) {
         case UserStatusOnline:
           return Text(client.getTranslation("lng_status_online"),
-              style: TextDisplay.create(size: 16, textColor: ClientTheme.currentTheme.getField("ActionBarOnlineTextColor")));
+              style: TextDisplay.create(
+                  size: 16,
+                  textColor: ClientTheme.currentTheme
+                      .getField("ActionBarOnlineTextColor")));
         case UserStatusEmpty:
           return const SizedBox.shrink();
         case UserStatusOffline:
-          var lastOnline = unixToDateTime((status as UserStatusOffline).wasOnline!);
+          var lastOnline =
+              unixToDateTime((status as UserStatusOffline).wasOnline!);
           var now = DateTime.now();
           var delta = (DateTime.now().difference(lastOnline) +
               (const Duration(days: 1) -
-                  Duration(hours: now.hour, minutes: now.minute, seconds: now.second, milliseconds: now.millisecond)));
+                  Duration(
+                      hours: now.hour,
+                      minutes: now.minute,
+                      seconds: now.second,
+                      milliseconds: now.millisecond)));
           if (delta.inDays <= 0) {
-            text = client.getTranslation("lng_status_lastseen_today", replacing: {"{time}": getHHMM(lastOnline)});
+            text = client.getTranslation("lng_status_lastseen_today",
+                replacing: {"{time}": getHHMM(lastOnline)});
           } else if (delta.inDays <= 1) {
-            text = client.getTranslation("lng_status_lastseen_yesterday", replacing: {"{time}": getHHMM(lastOnline)});
+            text = client.getTranslation("lng_status_lastseen_yesterday",
+                replacing: {"{time}": getHHMM(lastOnline)});
           } else if (delta.inDays <= 7) {
             text = client.getTranslation("lng_status_last_week");
           } else {
--- lib/Widgets/Chat/chat_display.dart
+++ lib/Widgets/Chat/chat_display.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/State managment/ui_events.dart';
 import 'package:myapp/Themes engine/theme_interpreter.dart';
-import 'package:myapp/UIManager.dart';
 import 'package:myapp/Widgets/Chat/action_bar_display.dart';
 import 'package:myapp/Widgets/Chat/input_field.dart';
 import 'package:myapp/Widgets/Chat/message_list.dart';
@@ -22,86 +20,98 @@ class ChatDisplay extends StatelessWidget {
 
   static const bool tw1nkleeModeEnable = false;
 
+  ///TODO get value from settings
+  static const bool useBlurBar = true;
+
   @override
   Widget build(BuildContext context) {
     var chat = client.getChat(chatId);
     return Stack(
       children: [
-        if (tw1nkleeModeEnable)
-          BackgroundDisplay(
-            client: client,
-            background: Background(
-              type: BackgroundTypeFill(
-                fill: BackgroundFillSolid(
-                  color: (ClientTheme.currentTheme
-                          .getField("tw1nkleeModeBackgroundColor") as Color)
-                      .value,
+        Stack(
+          children: [
+            if (tw1nkleeModeEnable)
+              BackgroundDisplay(
+                client: client,
+                background: Background(
+                  type: BackgroundTypeFill(
+                    fill: BackgroundFillSolid(
+                      color: (ClientTheme.currentTheme
+                              .getField("tw1nkleeModeBackgroundColor") as Color)
+                          .value,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          )
-        else
-          StreamBuilder(
-            stream: client.selectedBackground,
-            builder: (_, data) {
-              bool useDark =
-                  ClientTheme.currentTheme.environmentVariables["theme"]!() ==
+              )
+            else
+              StreamBuilder(
+                stream: client.selectedBackground,
+                builder: (_, data) {
+                  bool useDark = ClientTheme
+                          .currentTheme.environmentVariables["theme"]!() ==
                       "dark";
-              var background = client.getCachedBackground(useDark);
-              if (background == null) {
-                client.send(GetBackgrounds()).then(
-                      (backs) => client.send(
-                        SetBackground(
-                          forDarkTheme: useDark,
-                          background: InputBackgroundRemote(
-                            backgroundId: backs is Backgrounds
-                                ? backs.backgrounds![3].id!
-                                : (backs as Background).id!,
+                  var background = client.getCachedBackground(useDark);
+                  if (background == null) {
+                    client.send(GetBackgrounds()).then(
+                          (backs) => client.send(
+                            SetBackground(
+                              forDarkTheme: useDark,
+                              background: InputBackgroundRemote(
+                                backgroundId: backs is Backgrounds
+                                    ? backs.backgrounds![3].id!
+                                    : (backs as Background).id!,
+                              ),
+                            ),
                           ),
+                        );
+                    return const SizedBox.shrink();
+                  }
+                  return BackgroundDisplay(
+                    client: client,
+                    //TODO correct work with dark and light themes
+                    background: background,
+                  );
+                },
+              ),
+            Column(
+              children: [
+                Flexible(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                        child: MessageList(
+                          key: Key("chat?chatId=$chatId"),
+                          chatId: chat.id!,
+                          client: client,
                         ),
                       ),
-                    );
-                return const SizedBox.shrink();
-              }
-              return BackgroundDisplay(
-                client: client,
-                //TODO correct work with dark and light themes
-                background: background,
-              );
-            },
-          ),
-        Column(
-          children: [
-            ActionBarDisplay(
-              key: Key("actionBarDisplay?chatId=$chatId"),
-              client: client,
-              chat: chat,
-              onChatRevert: onChatRevert,
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
-                child: MessageList(
-                  key: Key("chat?chatId=$chatId"),
-                  chatId: chat.id!,
-                  client: client,
+                      ActionBarDisplay(
+                        key: Key("actionBarDisplay?chatId=$chatId"),
+                        client: client,
+                        chat: chat,
+                        onChatRevert: onChatRevert,
+                        useBlurStyle: useBlurBar,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 8,
+                    right: 8,
+                    left: 8,
+                  ),
+                  child: InputField(
+                    key: Key("inputField?chatId=$chatId"),
+                    client: client,
+                    chatId: chat.id!,
+                  ),
+                )
+              ],
             ),
-            Container(
-              margin: const EdgeInsets.only(
-                bottom: 8,
-                right: 8,
-                left: 8,
-              ),
-              child: InputField(
-                key: Key("inputField?chatId=$chatId"),
-                client: client,
-                chatId: chat.id!,
-              ),
-            )
           ],
-        )
+        ),
       ],
     );
   }
